3f1f586ba26f0a53b06474c3be5033b25668965b
Takashi K <tak.km19913@gmail.com>
2018-10-06 22:05:18 +0900

    1st Summary (Step1 to 4)

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# SDR-3
+# SDR-3 firmware
+Modification step by step.
+
+##Step1:
+- おじさん工房のHPに掲載されているパッチの適用
+
+##Step2:
+- CWモード(CW Lower と CW Upper)を追加
+- LCD上に表示されている周波数が送信周波数となる
+- PTTを押すと送信となる
+- CW波のON/OFF時に窓関数は使用していない(CW波は急峻に立ち上がり/立下りする)。
+
+##Step3:
+- CWキーイング機能を追加
+- CW key に PA5 を 使用。R2 に 3.3kΩを実装。
+- PTT で送信状態にして、PA5をGNDに落とすとCW波の送信となる。
+- CW波のON/OFFは窓関数(raised cosine)を使用(CW波は緩やかに立ち上がり/立下りする)。
+
+##Step4:
+- iambicキーヤー機能を追加
+- Dot key に PA5 を、 Dash key に PA6 を使用。 R3 に 3.3kΩを実装する
+- PA5/PA6をGNDに落とすとDot/DashのCW波が送信される。
+- USB端子(PA11)にSidetone信号が出力される(500Hz 矩形波)ので圧電スピーカーをつないでモニタできる。PA11と圧電スピーカとの間には直列に抵抗(1kΩなど)を入れる。
+- Keyerのスピードとブレークイン時間についてメニューは未実装。変更したい場合は、 ソースコードの define の値を修正する必要がある。
+- keyerフォルダは、SSB_trxフォルダに入れる
---
+++ firmware/modification step by step/Step4_add_iambic_keyer/keyer/iambic.c
@@ -0,0 +1,181 @@
+//
+//  Iambic Keyer for SDR-3
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// (C) JI1UDD 2018
+
+#include "board/board.h"
+#include "iambic.h"
+
+#define RejectTime      5           //   5ms, chattering rejection time
+#define AdjKeyTime      2           //   2ms, adjust key capture timing
+#define RELAY_DLY      15           //  15ms, change over time for T/R relay when dot/dash keys are pressed
+#define DotTime        50           //  50ms, dot pulse width,  DotTime(ms) = 1200 / WPM
+#define BreakinTime   300           // 300ms, brake-in time after finished Tx
+#define AdjDashTime     0           //   0ms, dash weight time
+#define DashTime      DotTime * 3
+
+#define STATE_RX        0
+#define STATE_DOT_PRE   1
+#define STATE_DOT_ON    2
+#define STATE_DOT_OFF   3
+#define STATE_DASH_PRE  4
+#define STATE_DASH_ON   5
+#define STATE_DASH_OFF  6
+#define STATE_TR_DLY    7
+
+int cw_key_on ;                    // CW keying control
+int cw_ptt_on ;                    // CW PTT control
+
+static int state       = STATE_RX ;
+static int state_delay = 0 ;
+static int dotcnt      = 0 ;
+static int dashcnt     = 0 ;
+static int doton       = 0 ;
+static int dashon      = 0 ;
+
+void iambic( void ) {
+  if ( state==STATE_DOT_ON )
+    dotcnt = 0 ;
+  else if (!(( state==STATE_DOT_OFF )&&( state_delay >= ((DotTime>>2)+AdjKeyTime)) )) {
+    if ( dotcnt < RejectTime ) {
+      if ( !DOT_PRESSED )
+        dotcnt = 0 ;
+      else
+        ++dotcnt ;
+    }
+  }
+  if (( state==STATE_DASH_ON ) && ( state_delay >= DotTime )) dotcnt = 0 ;
+  doton  = (dotcnt==RejectTime) ;
+
+  if ( state==STATE_DASH_ON )
+    dashcnt = 0 ;
+  else if (!(( state==STATE_DASH_OFF )&&( state_delay >= ((DotTime>>2)+AdjKeyTime)) )) {
+    if ( dashcnt < RejectTime ) {
+      if ( !DASH_PRESSED )
+        dashcnt = 0 ;
+      else
+        ++dashcnt ;
+    }
+  }
+  if (( state==STATE_DOT_ON ) && ( state_delay >= ((DotTime>>2)+AdjKeyTime) )) dashcnt = 0 ;
+  dashon = (dashcnt==RejectTime) ;
+
+  switch ( state ) {
+    case STATE_RX :
+      if ( doton ) {
+        state = STATE_DOT_PRE ;
+        state_delay = RELAY_DLY ;
+      } else if ( dashon ) {
+        state = STATE_DASH_PRE ;
+        state_delay = RELAY_DLY ;
+      }
+      break ;
+
+    case STATE_DOT_PRE :
+      if ( state_delay==0 ) {
+        state = STATE_DOT_ON ;
+        state_delay = DotTime ;
+      } else {
+        state_delay-- ;
+      }
+      break ;
+
+    case STATE_DOT_ON :
+      if ( state_delay==0 ) {
+        state = STATE_DOT_OFF ;
+        state_delay = DotTime ;
+      } else {
+        state_delay-- ;
+      }
+      break ;
+
+    case STATE_DOT_OFF :
+      if ( state_delay==0 ) {
+        if ( dashon ) {
+          state = STATE_DASH_ON ;
+          state_delay = DashTime + AdjDashTime ;
+        } else if ( doton ) {
+          state = STATE_DOT_ON ;
+          state_delay = DotTime ;
+        } else {
+          state = STATE_TR_DLY ; 
+          state_delay = BreakinTime ;
+        }
+      } else {
+        state_delay-- ;
+      }
+      break ;
+
+    case STATE_DASH_PRE :
+      if ( state_delay==0 ) {
+        state = STATE_DASH_ON ;
+        state_delay = DashTime + AdjDashTime ;
+      } else {
+        state_delay--;
+      }
+      break ;
+
+    case STATE_DASH_ON :
+      if ( state_delay==0 ) {
+        state = STATE_DASH_OFF ;
+        state_delay = DotTime ;
+      } else {
+        state_delay-- ;
+      }
+      break ;
+
+    case STATE_DASH_OFF :
+      if ( state_delay==0 ) {
+        if ( doton ) {
+          state = STATE_DOT_ON ;
+          state_delay = DotTime ;
+        } else if ( dashon ) {
+	      state = STATE_DASH_ON ;
+          state_delay = DashTime + AdjDashTime ;
+  	    } else {
+          state = STATE_TR_DLY ;
+          state_delay = BreakinTime ;
+        }
+      } else {
+        state_delay-- ;
+      }
+      break ;
+
+    case STATE_TR_DLY :
+      if ( state_delay==0 ) {
+        state = STATE_RX ;
+      } else if ( doton ) {
+        state = STATE_DOT_ON ;
+        state_delay =  DotTime ; //
+      } else if ( dashon ) {
+        state = STATE_DASH_ON ;
+        state_delay = DashTime + AdjDashTime ; //
+      } else {
+        state_delay-- ;
+      }
+      break ;
+  }
+
+  cw_ptt_on = !(state==STATE_RX) ;
+  cw_key_on = ((state==STATE_DOT_ON) || (state==STATE_DASH_ON)) ;
+
+  if ( cw_key_on ) {
+    BEEP_TOGGLE() ;
+  } else {
+    BEEP_OFF() ;
+  }
+}
